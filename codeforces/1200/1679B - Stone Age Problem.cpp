dd724738760dcd804e1c20d8dcc0d6e34ea619b9
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-22 23:34:37 +0530

    Add Stone Age Problem (1200)

--- codeforces/1200/1679B - Stone Age Problem.cpp
+++ codeforces/1200/1679B - Stone Age Problem.cpp
@@ -1,68 +1,90 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <iomanip>
+#include <climits>
+#include <cstring>
+
 using namespace std;
- 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
- 
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
     int n, q;
     cin >> n >> q;
- 
-    vector<long long> a(n + 1), vis(n + 1, 0);
- 
-    long long sum = 0;
- 
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> a[i];
-        sum += a[i];
-    }
- 
-    long long last = 0;
-    long long lastTime = -1;
-    long long timer = 0;
- 
-    while (q--)
-    {
-        timer++;
- 
-        int t;
-        cin >> t;
- 
-        if (t == 1)
-        {
-            int i;
-            long long x;
-            cin >> i >> x;
- 
-            long long old;
- 
-            if (vis[i] > lastTime)
-                old = a[i];
+
+    vector<pair<int, int>> values(n, {0, 0});
+
+    for (int i = 0; i < n; i++)
+        cin >> values[i].first;
+
+    pair<int, int> globalValue = {0, -1};
+
+    ll sum = 0;
+
+    for (int i = 0; i < n; i++)
+        sum += values[i].first;
+
+    for (int query = 1; query <= q; query++) {
+        int type;
+        cin >> type;
+
+        if (type == 1) {
+            int index, value;
+            cin >> index >> value;
+
+            index--;
+
+            if (values[index].second > globalValue.second)
+                sum += value - values[index].first;
             else
-                old = last;
- 
-            sum -= old;
-            sum += x;
- 
-            a[i] = x;
-            vis[i] = timer;
- 
-            cout << sum << '\n';
+                sum += value - globalValue.first;
+
+            values[index].first = value;
+            values[index].second = query;
         }
-        else
-        {
-            long long x;
-            cin >> x;
- 
-            last = x;
-            lastTime = timer;
-            sum = 1LL * n * x;
- 
-            cout << sum << '\n';
+        else {
+            int value;
+            cin >> value;
+
+            globalValue.first = value;
+            globalValue.second = query;
+
+            sum = 1LL * value * n;
         }
+
+        cout << sum << '\n';
     }
- 
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
     return 0;
 }
