7aa0d547d7cf0b4e15aef09cb6b327d8a47b49ab
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-08-24 22:29:50 +0530

    Add THU Packing Puzzle (1300)

---
+++ codeforces/1300/2216B - THU Packing Puzzle.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <iomanip>
+#include <climits>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+
+#define pb push_back
+#define ff first
+#define ss second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    ll t, h, u;
+    cin >> t >> h >> u;
+
+    if (t > 2 * h + u)
+        cout << 2 * t + 3 * h + 2 * u + 1;
+    else
+        cout << 2 * t + 3 * h + 3 * u - min(t, u);
+
+    cout << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
