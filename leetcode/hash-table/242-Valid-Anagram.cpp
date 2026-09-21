3c7b261dc7bbb220c2f0e5b1536233b167c6af1c
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-09-21 06:07:39 +0530

    Add Valid-Anagram (hash-table)

--- leetcode/hash-table/242-Valid-Anagram.cpp
+++ leetcode/hash-table/242-Valid-Anagram.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) return false;
-
-        sort(s.begin(), s.end());
-        sort (t.begin(), t.end());
-        if(s==t){
-            return true;
-        }
-        else {
+        if(s.length()!=t.length()){
             return false;
         }
-       
-          
+        // unordered_map<char,int> counts;
+        // unordered_map<char,int> countt;
+        // for(int i=0;i<s.length();i++){
+        //     counts[s[i]]++;
+        //     countt[t[i]]++;
+        // }
+        // return counts==countt;
+        vector<int> count(26,0);
+        for(int i=0;i<s.length();i++){
+            count[s[i]-'a']++;
+            count[t[i]-'a']--;
+        }
+        for(int x:count){
+            if(x!=0){
+                return false;
+            }
+        }
+        return true;
     }
-   
 };
