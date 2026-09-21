a03c9f9c7b3325ad0a97ed69d904ff989f801f25
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-09-21 07:08:03 +0530

    Add Longest-Common-Prefix (trie)

--- leetcode/trie/14-Longest-Common-Prefix.cpp
+++ leetcode/trie/14-Longest-Common-Prefix.cpp
@@ -2,16 +2,27 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         string prefix = strs[0];
+        // for (int i = 1; i < strs.size(); i++) {
+        //     while (strs[i].find(prefix) != 0) {
+        //         prefix.pop_back();
 
-        for (int i = 1; i < strs.size(); i++) {
-            while (strs[i].find(prefix) != 0) {
-                prefix.pop_back();
+        //         if (prefix.empty())
+        //             return "";
+        //     }
+        // }
 
-                if (prefix.empty())
-                    return "";
+        // return prefix;
+        for(int i=1;i<strs.size();i++){
+            int j=0;
+            while(j< min(prefix.length(),strs[i].length())){
+                if(prefix[j]!=strs[i][j]){
+                    break;
+                }
+                j++;
             }
+            prefix=prefix.substr(0,j);
         }
-
         return prefix;
+
     }
 };
