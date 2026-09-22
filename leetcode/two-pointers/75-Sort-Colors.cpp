ed9999c475a9f8676e6056462730863ac6ce2a4c
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-09-22 23:35:14 +0530

    Add Sort-Colors (two-pointers)

--- leetcode/two-pointers/75-Sort-Colors.cpp
+++ leetcode/two-pointers/75-Sort-Colors.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // better
-        //  int cnt0 =0;
-        //  int cnt1=0;
-        //  int cnt2=0;
-        //  for(int i=0; i<nums.size(); i++){
-        //      if(nums[i]==0) cnt0++;
-        //      else if(nums[i]==1) cnt1++;
-        //      else cnt2++;
-        //  }
-        //  for(int i=0; i<cnt0; i++){ nums[i]=0 ;}
-        //  for(int i=cnt0;i<cnt0+cnt1;i++){ nums[i]=1;}
-        //  for(int i=cnt0+cnt1; i<nums.size();i++){nums[i]=2;}
-
-        int low = 0;
-        int mid = 0;
-        int high = nums.size() - 1;
-        while(mid<=high) {
-            if (nums[mid] == 0) {
-                swap(nums[low], nums[mid]);
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                mid++;
-            } else if (nums[mid] == 2) {
-                swap(nums[mid], nums[high]);
-                high--;
+        int cnt0=0,cnt1=0,cnt2=0;
+        for(int x:nums){
+            if(x==0){
+                cnt0++;
+            }
+            else if(x==1){
+                cnt1++;
+            }
+            else{
+                cnt2++;
             }
         }
+        for(int i=0;i<cnt0;i++) nums[i]=0;
+        for(int i=cnt0; i<cnt0+cnt1;i++) nums[i]=1;
+        for(int i=cnt0+cnt1; i<nums.size(); i++) nums[i]=2;
+
     }
 };
