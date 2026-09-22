70d185ab8409733986e36e602b33beb8d429bfb1
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-09-22 17:54:31 +0530

    Add Design-HashMap (linked-list)

---
+++ leetcode/linked-list/706-Design-HashMap.cpp
@@ -0,0 +1,31 @@
+class MyHashMap {
+    private:
+       vector<int> nums;
+public:
+    MyHashMap() : nums(1000001,-1) {
+        
+    }
+    
+    void put(int key, int value) {
+        nums[key]=value;
+        
+    }
+    
+    int get(int key) {
+        return nums[key];
+        
+    }
+    
+    void remove(int key) {
+        nums[key]=-1;
+        
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
