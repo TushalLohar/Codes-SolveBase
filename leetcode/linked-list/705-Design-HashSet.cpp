4dd6766c0826f730c52ad05beb9ba6b5d4ff6cdf
Tushal Narayanji Lohar <tushallohar5@gmail.com>
2026-09-22 17:43:03 +0530

    Add Design-HashSet (linked-list)

---
+++ leetcode/linked-list/705-Design-HashSet.cpp
@@ -0,0 +1,30 @@
+class MyHashSet {
+    vector<bool> nums;
+public:
+    MyHashSet() {
+        nums.resize(1000001,false);
+        
+    }
+    
+    void add(int key) {
+        nums[key]=true;
+    }
+    
+    void remove(int key) {
+        nums[key]=false;
+        
+    }
+    
+    bool contains(int key) {
+        return nums[key];
+        
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
